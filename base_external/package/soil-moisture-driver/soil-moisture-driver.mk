296471d651417decc9e25bef04aebf8fb7539084
Christian Bull <cbull.projects@gmail.com>
2025-10-27 21:27:43 -0600

    rename driver

--- base_external/package/soil-moisture-driver/soil-moisture-driver.mk
+++ base_external/package/soil-moisture-driver/soil-moisture-driver.mk
@@ -1,13 +1,13 @@
-SOIL_MOISTURE_DRIVER_VERSION = 1b4d0d45ca7681a51c885162dc05288fa12811cc
+SOIL_MOISTURE_DRIVER_VERSION = 8ca4a8a055ae30b5db5e4b9ae76fac9dd106e30f
 SOIL_MOISTURE_DRIVER_SITE = git@github.com:Christian-Bull/soil-moisture-sensor.git
 SOIL_MOISTURE_DRIVER_SITE_METHOD = git
-SOIL_MOISTURE_DRIVER_GIT_SUBMODULES = YES
 
 SOIL_MOISTURE_DRIVER_MODULE_SUBDIRS = driver
+SOIL_MOISTURE_DRIVER_INSTALL_TARGET = YES
 
 define SOIL_MOISTURE_DRIVER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_load $(TARGET_DIR)/etc/init.d/S99soildriver
-	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_unload $(TARGET_DIR)/etc/init.d/K99soildriver
+	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_load.sh $(TARGET_DIR)/etc/init.d/S99soildriver
+	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_unload.sh $(TARGET_DIR)/etc/init.d/K99soildriver
 endef
 
 $(eval $(kernel-module))
