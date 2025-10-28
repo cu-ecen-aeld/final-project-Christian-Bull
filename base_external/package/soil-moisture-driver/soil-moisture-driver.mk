2603d6c1612094d1bc32e46e7ac2f5694d9b1564
Christian Bull <56417349+Christian-Bull@users.noreply.github.com>
2025-10-27 21:48:11 -0600

    4 add custom driver as a package to buildroot (#3)
    * add soil moisture package
    
    * rename driver
    
    * udate soil driver version

--- base_external/package/soil-moisture-driver/soil-moisture-driver.mk
+++ base_external/package/soil-moisture-driver/soil-moisture-driver.mk
@@ -1,14 +1,14 @@
-# AESD_CHAR_DRIVER_VERSION = 3bda09f46571827385d3eef5de413b0c13ab8bc9
-# AESD_CHAR_DRIVER_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-Christian-Bull.git
-# AESD_CHAR_DRIVER_SITE_METHOD = git
-# # AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
+SOIL_MOISTURE_DRIVER_VERSION = 488032743559a9d7700930125cc11fb0f34e2dac
+SOIL_MOISTURE_DRIVER_SITE = git@github.com:Christian-Bull/soil-moisture-sensor.git
+SOIL_MOISTURE_DRIVER_SITE_METHOD = git
 
-# AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+SOIL_MOISTURE_DRIVER_MODULE_SUBDIRS = driver
+SOIL_MOISTURE_DRIVER_INSTALL_TARGET = YES
 
-# define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
-# 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/etc/init.d/S99aesdchar
-# 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/etc/init.d/K99aesdchar
-# endef
+define SOIL_MOISTURE_DRIVER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_load.sh $(TARGET_DIR)/etc/init.d/S99soildriver
+	$(INSTALL) -m 0755 $(@D)/driver/soil-driver_unload.sh $(TARGET_DIR)/etc/init.d/K99soildriver
+endef
 
-# $(eval $(kernel-module))
-# $(eval $(generic-package))
+$(eval $(kernel-module))
+$(eval $(generic-package))
