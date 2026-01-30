37d40320085735991f5670abf5f7c7c28f238e87
Christian Bull <cbull.projects@gmail.com>
2026-01-30 13:04:55 -0700

    final changes to setup automations, server and sensor scraper

--- base_external/package/soil-moisture-sensor/soil-moisture-sensor.mk
+++ base_external/package/soil-moisture-sensor/soil-moisture-sensor.mk
@@ -1,4 +1,4 @@
-SOIL_MOISTURE_SENSOR_VERSION = e0150065b53de3b0ea49d1edc4a4565608661f31
+SOIL_MOISTURE_SENSOR_VERSION = 54e97305299831c2e53dd2559a5711a20d88050d
 SOIL_MOISTURE_SENSOR_SITE = git@github.com:Christian-Bull/soil-moisture-server.git
 SOIL_MOISTURE_SENSOR_SITE_METHOD = git
 SOIL_MOISTURE_SENSOR_GIT_SUBMODULES = YES
@@ -8,21 +8,22 @@ define SOIL_MOISTURE_SENSOR_BUILD_CMDS
 
 	cd $(@D)/server && \
 		$(TARGET_MAKE_ENV) \
-		GOOS=linux \
-		GOARCH=arm64 \
-		CGO_ENABLED=0 \
+		GOOS=linux GOARCH=arm64 CGO_ENABLED=0 \
 		$(HOST_DIR)/bin/go build \
 			-buildvcs=false \
 			-trimpath \
 			-ldflags "-s -w" \
 			-o $(@D)/sensor-server \
-			.
+			sensor-server.go
 endef
 
 define SOIL_MOISTURE_SENSOR_INSTALL_TARGET_CMDS
 	cp -r $(@D)/sensor-scraper/* $(TARGET_DIR)/bin/
 
 	$(INSTALL) -D -m 0755 $(@D)/sensor-server $(TARGET_DIR)/usr/bin/sensor-server
+
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0755 $(@D)/server/start-server.sh $(TARGET_DIR)/etc/init.d/S87sensorserver
 endef
 
 $(eval $(generic-package))
